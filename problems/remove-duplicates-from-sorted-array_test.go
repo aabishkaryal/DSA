e18eccdd1949e9bfad46de1138950b372a1da414
Aabishkar Aryal <aabishkaryal@gmail.com>
2022-08-20 20:20:13 +0545

    Added helper dependency for testing (#17)
    - Using the testify dependency and updated existing tests to use it.

--- problems/remove-duplicates-from-sorted-array_test.go
+++ problems/remove-duplicates-from-sorted-array_test.go
@@ -3,6 +3,8 @@ package problems_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/aabishkaryal/DSA/problems"
 )
 
@@ -15,14 +17,13 @@ func TestRemoveDuplicates(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
 		{name: "test1", args: args{nums: []int{1, 1, 2}}, want: 2},
 		{name: "test2", args: args{nums: []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}}, want: 5},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := problems.RemoveDuplicates(tt.args.nums); got != tt.want {
-				t.Errorf("RemoveDuplicates() = %v, want %v", got, tt.want)
+				require.Equal(t, tt.want, got)
 			}
 		})
 	}
