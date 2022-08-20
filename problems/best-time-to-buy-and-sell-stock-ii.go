f7117d764e2ff42c500b2e2541ebf52d2834b288
Aabishkar Aryal <aabishkaryal@gmail.com>
2022-08-20 13:16:03 +0545

    Configured golangci-lint (#13)
    * Configured golangci-lint
    
    * Fixed linter issues

--- problems/best-time-to-buy-and-sell-stock-ii.go
+++ problems/best-time-to-buy-and-sell-stock-ii.go
@@ -6,7 +6,7 @@ func MaxProfit(prices []int) int {
 	}
 	profit := 0
 	for i := 0; i < len(prices)-1; i++ {
-		// if today's price is lower than tomorrow's
+		// If today's price is lower than tomorrow's
 		// buy today and sell tomorrow making a profit
 		// repeat for the next day until the last.
 		if prices[i] < prices[i+1] {
