6e3d6c3116dc0da97ef9cca4692b5822d1f635a3
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-09-07 23:03:40 +0545

    Alternate solution if both the arrays were sorted in ascending order.

--- problems/intersection_of_two_arrays_ii.go
+++ problems/intersection_of_two_arrays_ii.go
@@ -17,3 +17,21 @@ func Intersect(nums1, nums2 []int) []int {
 	}
 	return result
 }
+
+func IntersectSorted(nums1, nums2 []int) []int {
+	result := make([]int, 0)
+	x, y := 0, 0
+	for x < len(nums1) && y < len(nums2) {
+		switch {
+		case nums1[x] == nums2[y]:
+			result = append(result, nums1[x])
+			x++
+			y++
+		case nums1[x] < nums2[y]:
+			x++
+		case nums2[y] < nums1[x]:
+			y++
+		}
+	}
+	return result
+}
--- problems/intersection_of_two_arrays_ii_test.go
+++ problems/intersection_of_two_arrays_ii_test.go
@@ -1,10 +1,12 @@
 package problems_test
 
 import (
+	"reflect"
 	"testing"
 
-	"github.com/aabishkaryal/DSA/problems"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/aabishkaryal/DSA/problems"
 )
 
 func TestIntersect(t *testing.T) {
@@ -26,6 +28,30 @@ func TestIntersect(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := problems.Intersect(tt.args.nums1, tt.args.nums2); !assert.ElementsMatch(t, got, tt.want) {
 				t.Errorf("Intersect() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIntersectSorted(t *testing.T) {
+	type args struct {
+		nums1 []int
+		nums2 []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{name: "test1", args: args{nums1: []int{1, 1, 2, 2}, nums2: []int{2, 2}}, want: []int{2, 2}},
+		{name: "test2", args: args{nums1: []int{4, 5, 9}, nums2: []int{4, 4, 9, 8, 9}}, want: []int{4, 9}},
+		{name: "test3", args: args{nums1: []int{1, 1, 2, 2}, nums2: []int{2}}, want: []int{2}},
+		{name: "test4", args: args{nums1: []int{1, 1, 2, 2}, nums2: []int{3}}, want: []int{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := problems.IntersectSorted(tt.args.nums1, tt.args.nums2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Testname: %s, IntersectSorted() = %v, want %v", tt.name, got, tt.want)
 			}
 		})
 	}
