42261c4271a731854051f29b800bd2c80257af7c
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-09-07 23:01:13 +0545

    Increased efficiency of the algorithm by only caching one of the arrays

--- problems/intersection_of_two_arrays_ii.go
+++ problems/intersection_of_two_arrays_ii.go
@@ -3,28 +3,17 @@ package problems
 func Intersect(nums1, nums2 []int) []int {
 	result := make([]int, 0)
 	countNums1 := make(map[int]int)
-	countNums2 := make(map[int]int)
 
 	for _, num := range nums1 {
 		countNums1[num]++
 	}
 
 	for _, num := range nums2 {
-		countNums2[num]++
-	}
-
-	for num, count := range countNums1 {
-		repeatCount := min(count, countNums2[num])
-		for i := 0; i < repeatCount; i++ {
+		count, ok := countNums1[num]
+		if ok && count > 0 {
+			countNums1[num]--
 			result = append(result, num)
 		}
 	}
 	return result
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
 }
