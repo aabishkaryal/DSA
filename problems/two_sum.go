120f494fc27d131faddbd8392496af1ae56017da
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-09-12 22:58:12 +0545

    Solved with sorting and searching with two pointers

--- problems/two_sum.go
+++ problems/two_sum.go
@@ -1,14 +1,36 @@
 package problems
 
+import "sort"
+
 func TwoSum(nums []int, target int) []int {
-	cache := make(map[int]int)
-	for i, num := range nums {
-		valueNeeded := target - num
-		index, ok := cache[valueNeeded]
-		if ok {
-			return []int{i, index}
+	original := append([]int{}, nums...)
+	sort.Ints(nums)
+	lhs, rhs := 0, len(nums)-1
+	for lhs < rhs {
+		switch sum := nums[lhs] + nums[rhs]; {
+		case sum == target:
+			n1, n2 := nums[lhs], nums[rhs]
+			return findIndex(original, n1, n2)
+		case sum < target:
+			lhs++
+		default:
+			rhs--
 		}
-		cache[num] = i
 	}
 	return []int{}
+}
+
+func findIndex(nums []int, n1, n2 int) []int {
+	i1, i2 := -1, -1
+	for i, num := range nums {
+		if i1 < 0 && num == n1 {
+			i1 = i
+		} else if i2 < 0 && num == n2 {
+			i2 = i
+		}
+		if i1 >= 0 && i2 >= 0 {
+			break
+		}
+	}
+	return []int{i1, i2}
 }
